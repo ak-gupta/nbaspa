2bfa763926594a0ee4c5daf0bb8277f78eac5a46
Akshay Gupta <akshaykgupta@protonmail.com>
2021-02-12 17:07:18 -0500

    Changing package name to nbaspa

--- nba_survival/data/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Import the modules."""
-
-from typing import List
-
-from .factory import NBADataFactory
-
-__all__: List[str] = ["NBADataFactory"]
---
+++ nbaspa/data/__init__.py
@@ -0,0 +1,7 @@
+"""Import the modules."""
+
+from typing import List
+
+from .factory import NBADataFactory
+
+__all__: List[str] = ["NBADataFactory"]
