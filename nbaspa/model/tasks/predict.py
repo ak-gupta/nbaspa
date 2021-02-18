86f22ed0da98ba30cb5c8efc86eb5668b0559f0e
Akshay Gupta <akshaykgupta@protonmail.com>
2021-02-17 22:46:48 -0500

    A working model with some basic experimental results

--- nbaspa/model/tasks/predict.py
+++ nbaspa/model/tasks/predict.py
@@ -30,8 +30,11 @@ class Predict(Task):
         """
         if isinstance(model, CoxTimeVaryingFitter):
             return self._run_lifelines(model, data)
-        else:
+        elif isinstance(model, xgb.Booster):
             return self._run_xgboost(model, data)
+        else:
+            self.logger.error("No method specified for this model type")
+            raise NotImplementedError("No method specified for this model type")
 
     @staticmethod
     def _run_lifelines(model: CoxTimeVaryingFitter, data: pd.DataFrame) -> np.ndarray:
@@ -52,12 +55,12 @@ class Predict(Task):
         return model.predict_partial_hazard(data)
     
     @staticmethod
-    def _run_xgboost(model, data: pd.DataFrame) -> np.ndarray:
+    def _run_xgboost(model: xgb.Booster, data: pd.DataFrame) -> np.ndarray:
         """Get the partial hazard for an observation.
 
         Parameters
         ----------
-        model
+        model : xgb.Booster
             The fitted model.
         data : pd.DataFrame
             The input dataframe.
@@ -93,6 +96,8 @@ class WinProbability(Task):
             return self._run_lifelines(model=model, data=data)
         elif isinstance(model, str) and model == "nba":
             return data
+        elif isinstance(model, xgb.Booster):
+            return self._run_xgboost(model=model, data=data)
         else:
             self.logger.error("No method specified for this model type")
             raise NotImplementedError("No method specified for this model type")
@@ -101,7 +106,7 @@ class WinProbability(Task):
     def _run_lifelines(
         model: CoxTimeVaryingFitter,
         data: pd.DataFrame
-    ) -> np.ndarray:
+    ) -> pd.DataFrame:
         """Retrieve the win probability.
 
         Parameters
@@ -113,11 +118,63 @@ class WinProbability(Task):
         
         Returns
         -------
+        pd.DataFrame
+            The updated dataset.
         """
         # Get the cumulative hazard -- copying from ``lifelines.fitters.SemiParametericPHFitter``
         vals = model.predict_partial_hazard(data)
         c0 = interpolate_at_times(model.baseline_cumulative_hazard_, data["stop"].values)
         # Survival is the negative exponent of the cumulative hazard
         data[META["survival"]] = 1 - np.exp(-(c0 * vals.values))
+
+        return data
+    
+    @staticmethod
+    def _run_xgboost(
+        model: xgb.Booster,
+        data: pd.DataFrame
+    ) -> pd.DataFrame:
+        """Retrieve the win probability.
+
+        Parameters
+        ----------
+        model : xgb.Booster
+            The fitted XGBoost model.
+        data : pd.DataFrame
+            The input dataset to be evaluated.
+        
+        Returns
+        -------
+        np.ndarray
+            The updated dataset.
+        """
+        # Get the cumulative hazard using ``lifelines``
+        # First, get the partial hazard values
+        hazard = model.predict(xgb.DMatrix(data[META["static"] + META["dynamic"]]))
+        # Get the unique failure times
+        unique_death_times = np.unique(data.loc[data[META["event"]] == 1, "stop"].values)
+        baseline_hazard_ = pd.DataFrame(
+            np.zeros_like(unique_death_times),
+            index=unique_death_times,
+            columns=["baseline hazard"]
+        )
+
+        for t in unique_death_times:
+            ix = (data["start"].values < t) & (t <= data["stop"].values)
+
+            events_at_t = data[META["event"]].values[ix]
+            stops_at_t = data["stop"].values[ix]
+            hazards_at_t = hazard[ix]
+
+            deaths = events_at_t & (stops_at_t == t)
+
+            death_counts = deaths.sum()
+            baseline_hazard_.loc[t] = death_counts / hazards_at_t.sum()
+
+        cumulative_hazard_ = baseline_hazard_.cumsum()
+
+        # Get the cumulative probability
+        c0 = interpolate_at_times(cumulative_hazard_, data["stop"].values)
+        data[META["survival"]] = 1 - np.exp(-(c0 * hazard))
 
         return data
--- nbaspa/model/tasks/xgboost.py
+++ nbaspa/model/tasks/xgboost.py
@@ -1,6 +1,6 @@
 """Tasks for XGBoost."""
 
-from typing import Dict, Tuple
+from typing import Dict, Optional
 
 import numpy as np
 import pandas as pd
@@ -9,128 +9,17 @@ import xgboost as xgb
 
 from .meta import META
 
-def _ncox_loglik(
-    predt: np.ndarray,
-    label: np.ndarray,
-    stop: np.ndarray,
-    group: np.ndarray,
-) -> np.ndarray:
-    """Get the negative log-likelihood.
-
-    Parameters
-    ----------
-    predt : np.ndarray
-        The array of predicted values from the model.
-    label : np.ndarray
-        A boolean array of event labels.
-    stop : np.ndarray
-        A array of the right (inclusive) time range value for covariate values.
-    group : np.ndarray
-        The game indicator for each observation.
-    
-    Returns
-    -------
-    np.ndarray
-        The observation-level negative log likelihood values.
-    """
-    # Get the unique games
-    games = np.unique(group)
-    # Get the array index for every failure time (last row with game value)
-    ftimes = np.array(
-        [np.argwhere(group == game)[-1][0] for game in games]
-    )
-    # Get the sum -- each entry in the array has to be the sum of exponentiated predicted
-    # values for the game at the failure time.
-    exp_sum = np.ones(len(predt))
-    for index, ftime in enumerate(ftimes):
-        # Get the predicted value at failure time
-        exp_sum[index] = np.sum(np.exp(predt[stop == stop[ftime]]))
-    
-    return label * (predt - np.log(exp_sum))
-
-def _ncox_gradient(
-    predt: np.ndarray,
-    label: np.ndarray,
-    stop: np.ndarray,
-    group: np.ndarray,
-) -> np.ndarray:
-    """Get the negative gradient.
-
-    Parameters
-    ----------
-    predt : np.ndarray
-        The array of predicted values from the model.
-    label : np.ndarray
-        A boolean array of event labels.
-    stop : np.ndarray
-        A array of the right (inclusive) time range value for covariate values.
-    group : np.ndarray
-        The game indicator for each observation.
-    
-    Returns
-    -------
-    np.ndarray
-        The observation-level negative gradient values.
-    """
-    # Get the unique games
-    games = np.unique(group)
-    # Get the array index for every failure time (last row with game value)
-    ftimes = np.array(
-        [np.argwhere(group == game)[-1][0] for game in games]
-    )
-    # Get the sum -- each entry in the array has to be the sum of exponentiated predicted
-    # values for the game at the failure time.
-    exp_sum = np.ones(len(predt))
-    for index, ftime in enumerate(ftimes):
-        # Get the predicted value at failure time
-        exp_sum[index] = np.sum(np.exp(predt[stop == stop[ftime]]))
-    
-    return label * ((np.exp(predt) / exp_sum) - 1)
-
-def _ncox_hessian(
-    predt: np.ndarray,
-    label: np.ndarray,
-    stop: np.ndarray,
-    group: np.ndarray,
-) -> np.ndarray:
-    """Get the negative hessian.
-
-    Parameters
-    ----------
-    predt : np.ndarray
-        The array of predicted values from the model.
-    label : np.ndarray
-        A boolean array of event labels.
-    stop : np.ndarray
-        A array of the right (inclusive) time range value for covariate values.
-    group : np.ndarray
-        The game indicator for each observation.
-    
-    Returns
-    -------
-    np.ndarray
-        The observation-level negative hessian values.
-    """
-    # Get the unique games
-    games = np.unique(group)
-    # Get the array index for every failure time (last row with game value)
-    ftimes = np.array(
-        [np.argwhere(group == game)[-1][0] for game in games]
-    )
-    # Get the sum -- each entry in the array has to be the sum of exponentiated predicted
-    # values for the game at the failure time.
-    exp_sum = np.ones(len(predt))
-    for index, ftime in enumerate(ftimes):
-        # Get the predicted value at failure time
-        exp_sum[index] = np.sum(np.exp(predt[stop == stop[ftime]]))
-    
-    return (
-        label * ((np.exp(predt) / exp_sum) - ((np.exp(predt) ** 2) / (exp_sum ** 2)))
-    )
 
 class FitXGBoost(Task):
     """Fit the XGBoost model."""
-    def run(self, params: Dict, train_data: pd.DataFrame, **kwargs):
+    def run(
+        self,
+        train_data: pd.DataFrame,
+        params: Optional[Dict] = None,
+        stopping_data: Optional[pd.DataFrame] = None,
+        seed: Optional[int] = 42,
+        **kwargs
+    ) -> xgb.Booster:
         """Fit the XGBoost model.
 
         This model will be fitted using a custom objective function.
@@ -141,51 +30,47 @@ class FitXGBoost(Task):
             The parameters for the model.
         train_data : pd.DataFrame
             The training data, from ``SurvivalData.run()``.
+        stopping_data : pd.DataFrame, optional (default None)
+            Early stopping data, from ``SurvivalData.run()``.
+        seed : int, optional (default 42)
+            Seed.
         **kwargs
             Keyword arguments for ``xgb.train``.
         
         Returns
         -------
+        xgb.Booster
+            The fitted model
         """
-        # Get the game identifiers since they aren't easily defined in ``xgb.DMatrix``
-        group = pd.get_dummies(train_data[META["id"]]).values.argmax(1)
-
-        # Define a custom objective
-        def objective(predt: np.ndarray, dtrain: xgb.DMatrix) -> np.ndarray:
-            """Cox custom objective function."""
-            label = dtrain.get_label()
-            stop = dtrain.get_float_info("label_upper_bound")
-
-            grad = _ncox_gradient(predt=predt, label=label, stop=stop, group=group)
-            hess = _ncox_hessian(predt=predt, label=label, stop=stop, group=group)
-
-            return grad, hess
-        
-        # Define the evaluation metric
-        def eval_metric(predt: np.ndarray, dtrain: xgb.DMatrix) -> np.ndarray:
-            """Cox custom evaluation metric."""
-            label = dtrain.get_label()
-            stop = dtrain.get_float_info("label_upper_bound")
-
-            return "cox-nloglik", np.sum(_ncox_loglik(predt=predt, label=label, stop=stop, group=group))
-        
-        # Train the model
-        params.update(
-            {"disable_default_eval_metric": 1}
-        )
+        # Create datasets
+        self.logger.info("Converting training data to ``xgb.DMatrix``")
+        train = train_data.copy()
+        train.loc[train[META["event"]] == 0, "stop"] = -train["stop"]
         dtrain = xgb.DMatrix(
-            train_data[META["static"] + META["dynamic"]], train_data[META["event"]]
+            train[META["static"] + META["dynamic"]], train["stop"]
         )
-        dtrain.set_float_info("label_lower_bound", train_data["start"])
-        dtrain.set_float_info("label_upper_bound", train_data["stop"])
+        evals = [(dtrain, "train"),]
+        if stopping_data is not None:
+            self.logger.info("Converting stopping data to ``xgb.DMatrix``")
+            stop = stopping_data.copy()
+            stop.loc[stop[META["event"]] == 0, "stop"] = -stop["stop"]
+            dstop = xgb.DMatrix(
+                stop[META["static"] + META["dynamic"]], stop["stop"]
+            )
+            evals.append((dstop, "stopping"))
+
         self.logger.info("Training the model...")
+        initial_params = {
+            "objective": "survival:cox",
+            "seed": seed
+        }
+        initial_params.update(params)
         model = xgb.train(
-            params,
+            initial_params,
             dtrain,
-            obj=objective,
-            feval=eval_metric,
-            evals=[(dtrain, "train")],
+            evals=evals,
             **kwargs
         )
+        self.logger.info("Model training complete...")
 
         return model
