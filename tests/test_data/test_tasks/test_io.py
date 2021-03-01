e802356af39e43988440c817db882d2f4ed18124
Akshay Gupta <akshaykgupta@protonmail.com>
2021-03-01 17:18:24 -0500

    Getting the game log test to work

--- tests/test_data/test_tasks/test_io.py
+++ tests/test_data/test_tasks/test_io.py
@@ -5,7 +5,9 @@ from pathlib import Path
 
 from nbaspa.data.tasks import (
     GenericLoader,
-    PlayByPlayLoader
+    PlayByPlayLoader,
+    WinProbabilityLoader,
+    GameLogLoader
 )
 
 def test_load_scoreboard(data_dir, header, last_meeting):
@@ -29,23 +31,30 @@ def test_load_scoreboard(data_dir, header, last_meeting):
             last_meeting[last_meeting["GAME_ID"] == game].reset_index(drop=True)
         )
 
-def test_load_pbp(data_dir, pbp):
+def test_load_pbp(data_dir, header, pbp):
     """Test loading play-by-play data."""
-    for game, gamedate in [
-        ("00218DUMMY1", datetime(2018, 12, 25)),
-        ("00218DUMMY2", datetime(2018, 12, 26))
-    ]:
-        pre = GenericLoader(loader="Scoreboard")
-        score = pre.run(
-            output_dir=data_dir / Path("2018-19"),
-            GameDate=gamedate.strftime("%m/%d/%Y")
-        )
-        loader = PlayByPlayLoader()
-        output = loader.run(
-            header=score["GameHeader"],
-            output_dir=data_dir / Path("2018-19")
-        )
+    loader = PlayByPlayLoader()
+    output = loader.run(
+        header=header,
+        output_dir=data_dir / Path("2018-19")
+    )
 
-        assert output.equals(
-            pbp[pbp["GAME_ID"] == game].reset_index(drop=True)
-        )
+    assert output.equals(pbp)
+
+def test_win_probability_loader(data_dir, header, win_prob):
+    """Test loading NBA win probability data."""
+    loader = WinProbabilityLoader()
+    output = loader.run(
+        header=header,
+        output_dir=data_dir / Path("2018-19")
+    )
+    assert output.equals(win_prob)
+
+def test_gamelog_loader(data_dir, gamelog):
+    """Test loading gamelogs."""
+    loader = GameLogLoader()
+    output = loader.run(
+        season="2018-19",
+        output_dir=data_dir / Path("2018-19")
+    )
+    assert output.equals(gamelog)
