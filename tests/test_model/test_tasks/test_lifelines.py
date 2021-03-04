b31b9d57dd66703aa442fbe0d25fb580bfcc67a2
Akshay Gupta <akshaykgupta@protonmail.com>
2021-03-04 00:36:37 -0500

    Fixing lifelines test and travis file

--- tests/test_model/test_tasks/test_lifelines.py
+++ tests/test_model/test_tasks/test_lifelines.py
@@ -28,3 +28,34 @@ def test_fit_lifelines(mock_ll, data):
     _ = tsk.run(model=model, data=df)
 
     assert model.fit.call_count == 1
+    
+    args, kwargs = model.fit.call_args_list[0]
+
+    assert args[0].equals(
+        df[
+            [
+                "GAME_ID",
+                "WIN",
+                "start",
+                "stop",
+                "HOME_NET_RATING",
+                "VISITOR_NET_RATING",
+                "HOME_W_PCT",
+                "VISITOR_W_PCT",
+                "LAST_GAME_WIN",
+                "HOME_GAMES_IN_LAST_3_DAYS",
+                "HOME_GAMES_IN_LAST_5_DAYS",
+                "HOME_GAMES_IN_LAST_7_DAYS",
+                "VISITOR_GAMES_IN_LAST_3_DAYS",
+                "VISITOR_GAMES_IN_LAST_5_DAYS",
+                "VISITOR_GAMES_IN_LAST_7_DAYS",
+                "SCOREMARGIN",
+                "HOME_LINEUP_PLUS_MINUS",
+                "VISITOR_LINEUP_PLUS_MINUS"
+            ]
+        ]
+    )
+    assert kwargs["id_col"] == "GAME_ID"
+    assert kwargs["event_col"] == "WIN"
+    assert kwargs["start_col"] == "start"
+    assert kwargs["stop_col"] == "stop"
